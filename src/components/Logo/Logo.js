82410b272e8c8730111727bc672ee530774ac15d
sovvetbekovna.02@gmall.com <kaparbekovaariyana@gmail.com>
2021-03-18 14:17:50 +0600

    changed Logo.js

--- src/components/Logo/Logo.js
+++ src/components/Logo/Logo.js
@@ -4,7 +4,7 @@ import logotip from "../../images/logotip.png"
 const Logo = () => {
     return (
         <div className={classes.Logo}>
-            <img src={logotip} />
+            <img src={logotip} alt="#"/>
             <span>Bell</span>
             
         </div>
