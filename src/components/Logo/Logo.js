c36afd45e4151c9d8c7baa348745158e0238e581
sovvetbekovna.02@gmall.com <kaparbekovaariyana@gmail.com>
2021-03-18 14:14:55 +0600

    change Logo.js

--- src/components/Logo/Logo.js
+++ src/components/Logo/Logo.js
@@ -1,7 +1,7 @@
 import classes from "./Logo.module.css";
 import logotip from "../../images/logotip.png"
 
-const Logo = ({ }) => {
+const Logo = () => {
     return (
         <div className={classes.Logo}>
             <img src={logotip} />
