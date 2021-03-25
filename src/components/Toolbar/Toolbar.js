8344a10fc932d2d6da2ede448f9779cc95bee56d
sovvetbekovna.02@gmall.com <kaparbekovaariyana@gmail.com>
2021-03-25 14:04:32 +0600

    Clean Toolbar.js

--- src/components/Toolbar/Toolbar.js
+++ src/components/Toolbar/Toolbar.js
@@ -1,6 +1,7 @@
 import DrawerOpen from "../Drawer/DrawerOpen/DrawerOpen";
 import Logo from "../Logo/Logo";
 import Nav from "../Nav/Nav";
+
 import classes from "./Toolbar.module.css";
 
 const Toolbar = ({ openDrawer }) => {
