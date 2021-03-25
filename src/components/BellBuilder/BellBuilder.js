8c0edd41ffe3b419f80997cecf0fdfe63fb551d9
sovvetbekovna.02@gmall.com <kaparbekovaariyana@gmail.com>
2021-03-25 14:42:25 +0600

    Move BellBuider.js

--- src/components/BellBuilder/BellBuilder.js
+++ src/components/BellBuilder/BellBuilder.js
@@ -2,17 +2,18 @@ import BellPreview from "./BellPreview/BellPreview";
 import BellControls from "./BellControls/BellControls";
 
 import classes from "./BellBuilder.module.css";
+import { useState } from "react";
 
 
 const BellBuilder = () => {
-  const ingredients = {
+  const [ingredients, setIngredients] = useState({
     tomato: 20,
     salami: 20,
     greenOlive: 20,
     blackOlive: 20,
     redPepper: 20,
     yellowPepper: 15,
-  };
+  });
 
   return (
     <div className={classes.BellBuilder}>
