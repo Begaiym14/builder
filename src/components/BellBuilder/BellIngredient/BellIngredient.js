5a5589ff0edfb3cb33eba4e67e116a0dc2fa1a82
sovvetbekovna.02@gmall.com <kaparbekovaariyana@gmail.com>
2021-04-06 14:04:25 +0600

    Changes BellLngredient.js

--- src/components/BellBuilder/BellIngredient/BellIngredient.js
+++ src/components/BellBuilder/BellIngredient/BellIngredient.js
@@ -1,3 +1,4 @@
+import classes from "./BellIngredient.module.css";
 import salamiBackground from "../../../images/salami.svg";
 import tomatoBackground from "../../../images/tomato.svg";
 import blackOliveBackground from "../../../images/blackOlive.svg";
@@ -5,48 +6,48 @@ import greenOliveBackground from "../../../images/greenOlive.svg";
 import redPepperBackground from "../../../images/redPepper.svg";
 import yellowPepperBackground from "../../../images/yellowPepper.svg";
 
-import classes from "./BellIngredient.module.css";
+const BellIngredient = ({ type, fixed }) => {
+  const types = {
+    salami: { backgroundImage: `url(${salamiBackground})`, width: "35px", height: "35px" },
+    tomato: { backgroundImage: `url(${tomatoBackground})`, width: "35px", height: "35px" },
+    blackOlive: { backgroundImage: `url(${blackOliveBackground})`, width: "10px", height: "10px" },
+    greenOlive: { backgroundImage: `url(${greenOliveBackground})`, width: "10px", height: "10px" },
+    redPepper: { backgroundImage: `url(${redPepperBackground})`, width: "20px", height: "20px" },
+    yellowPepper: { backgroundImage: `url(${yellowPepperBackground})`, width: "40px", height: "40px" },
+  };
 
-const BellIngredient = ({ type }) => {
-    const types = {
-        salami: { backgroundImage: `url(${salamiBackground})`, width: "35px", height: "35px" },
-        tomato: { backgroundImage: `url(${tomatoBackground})`, width: "35px", height: "35px" },
-        blackOlive: { backgroundImage: `url(${blackOliveBackground})`, width: "10px", height: "10px" },
-        greenOlive: { backgroundImage: `url(${greenOliveBackground})`, width: "10px", height: "10px" },
-        redPepper: { backgroundImage: `url(${redPepperBackground})`, width: "20px", height: "20px" },
-        yellowPepper: { backgroundImage: `url(${yellowPepperBackground})`, width: "40px", height: "40px" },
-    };
+  function getPosition(ingredientWidth) {
+    const bellDiameter = 380;
+    const bellRadius = bellDiameter / 2;
+    const ingredientRadius = parseInt(ingredientWidth) / 2;
 
-    function getPosition(ingredientWidth) {
-        const bellDiameter = 380;
-        const bellRadius = bellDiameter / 2;
-        const ingredientRadius = parseInt(ingredientWidth) / 2;
+    const ingredientTop = Math.round(Math.random() * bellDiameter);
+    const ingredientLeft = Math.round(Math.random() * bellDiameter);
 
-        const ingredientTop = Math.round(Math.random() * bellDiameter);
-        const ingredientLeft = Math.round(Math.random() * bellDiameter);
+    const distance = Math.sqrt(
+      Math.pow(ingredientTop - bellRadius, 2) + Math.pow(ingredientLeft - bellRadius, 2)
+    ) + ingredientRadius;
 
-        const distance = Math.sqrt(
-            Math.pow(ingredientTop - bellRadius, 2) + Math.pow(ingredientLeft - bellRadius, 2)
-        ) + ingredientRadius;
+    return distance < bellRadius
+      ? {
+        top: ingredientTop - ingredientRadius,
+        left: ingredientLeft - ingredientRadius
+      }
+      : getPosition(ingredientWidth);
+  }
 
-        return distance < bellRadius
-            ? {
-                top: ingredientTop - ingredientRadius,
-                left: ingredientLeft - ingredientRadius
-            }
-            : getPosition(ingredientWidth);
-    }
-
-    // Get random position for this ingredient.
+  // Get random position for this ingredient.
+  if (!fixed) {
     const position = getPosition(types[type].width);
     types[type].top = position.top + "px";
     types[type].left = position.left + "px";
-    // Get random rotation for this ingredient.
-    types[type].transform = `rotate(${Math.round(Math.random() * 360)}deg)`;
+  }
+  // Get random rotation for this ingredient.
+  types[type].transform = `rotate(${Math.round(Math.random() * 360)}deg)`;
 
-    return (
-        <div className={classes.BellIngredient} style={types[type]}></div>
-    );
+  return (
+    <div className={classes.BellIngredient} style={types[type]}></div>
+  );
 }
 
 export default BellIngredient;
