d36cf8f6b01ab5bf7fc19cc05c415cf8cc9ef28f
sovvetbekovna.02@gmall.com <kaparbekovaariyana@gmail.com>
2021-04-06 14:03:57 +0600

    Change BellControl.js

--- src/components/BellBuilder/BellControls/BellControl/BellControl.js
+++ src/components/BellBuilder/BellControls/BellControl/BellControl.js
@@ -1,16 +1,16 @@
 import BellIngredient from "../../BellIngredient/BellIngredient";
 import classes from "./BellControl.module.css";
 
-const BellControl = (type, count) => {
-    return (
-        <div className={classes.BellControl}>
-            <button className={classes.more}>+</button>
-            <div className={classes.ingredient}>
-                <BellIngredient type={type} fixed />
-            </div>
-            <button className={classes.less}>-</button>
-        </div >
-    );
+const BellControl = ({ type, add, remove }) => {
+  return (
+    <div className={classes.BellControl}>
+      <button className={classes.more} onClick={() => add(type)}>+</button>
+      <div className={classes.ingredient}>
+        <BellIngredient type={type} fixed />
+      </div>
+      <button className={classes.less} onClick={() => remove(type)}>-</button>
+    </div>
+  );
 }
 
 export default BellControl;
