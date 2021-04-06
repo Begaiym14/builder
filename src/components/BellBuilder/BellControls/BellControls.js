bc75bbe8e73789e4a31fb468fed35b3917c65bf9
sovvetbekovna.02@gmall.com <kaparbekovaariyana@gmail.com>
2021-04-06 14:03:18 +0600

    change BellControls.js

--- src/components/BellBuilder/BellControls/BellControls.js
+++ src/components/BellBuilder/BellControls/BellControls.js
@@ -1,15 +1,19 @@
-  
 import BellControl from "./BellControl/BellControl";
 import classes from "./BellControls.module.css";
 
-const BellControls = ({ ingredients }) => {
+const BellControls = ({ ingredients, addIngredient, removeIngredient }) => {
   const results = [];
   for (const ingredient in ingredients) {
-    results.push(<BellControl type={ingredient} />)
+    results.push(<BellControl
+        key={ingredient}
+        add={addIngredient}
+        remove={removeIngredient}
+        type={ingredient} />)
   }
 
   return (
     <div className={classes.BellControls}>
+      <strong>Ingredients</strong>
       {results}
     </div>
   );
