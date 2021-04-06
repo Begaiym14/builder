63f4976ad25e1a64e55a06f84728d5e6b8d01fba
sovvetbekovna.02@gmall.com <kaparbekovaariyana@gmail.com>
2021-04-06 14:02:57 +0600

    change Builder.js

--- src/components/BellBuilder/BellBuilder.js
+++ src/components/BellBuilder/BellBuilder.js
@@ -4,21 +4,36 @@ import BellControls from "./BellControls/BellControls";
 import classes from "./BellBuilder.module.css";
 import { useState } from "react";
 
-
 const BellBuilder = () => {
   const [ingredients, setIngredients] = useState({
-    tomato: 20,
-    salami: 20,
-    greenOlive: 20,
-    blackOlive: 20,
-    redPepper: 20,
-    yellowPepper: 15,
+    tomato: 1,
+    salami: 1,
+    greenOlive: 1,
+    blackOlive: 1,
+    redPepper: 1,
+    yellowPepper: 1,
   });
+
+  function addIngredient(type) {
+    const newIngredients = { ...ingredients };
+    newIngredients[type]++;
+    setIngredients(newIngredients);
+  }
+
+  function removeIngredient(type) {
+    const newIngredients = { ...ingredients };
+    newIngredients[type]--;
+    setIngredients(newIngredients);
+  }
 
   return (
     <div className={classes.BellBuilder}>
       <BellPreview ingredients={ingredients} />
-      <BellControls />
+      <BellControls
+        ingredients={ingredients}
+        addIngredient={addIngredient}
+        removeIngredient={removeIngredient}
+        />
     </div>
   );
 }
