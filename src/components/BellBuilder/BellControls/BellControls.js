98f51144dd695b24649a9250cc8029386f45f67a
sovvetbekovna.02@gmall.com <kaparbekovaariyana@gmail.com>
2021-04-07 15:57:00 +0600

    to chnaged BellControls.js

--- src/components/BellBuilder/BellControls/BellControls.js
+++ src/components/BellBuilder/BellControls/BellControls.js
@@ -2,21 +2,31 @@ import Button from "../../Ul/Button/Button";
 import BellControl from "./BellControl/BellControl";
 import classes from "./BellControls.module.css";
 
-const BellControls = ({ ingredients, addIngredient, removeIngredient, canBuy }) => {
+const BellControls = ({
+  ingredients,
+  addIngredient,
+  removeIngredient,
+  startOrdering
+}) => {
   const results = [];
+  let total = 0;
   for (const ingredient in ingredients) {
+    // Add ingredient number to totals number
+    total += ingredients[ingredient];
+    // Render pizza control for this ingredient
     results.push(<BellControl
-      key={ingredient}
-      add={addIngredient}
-      remove={removeIngredient}
-      type={ingredient} />)
+        key={ingredient}
+        add={addIngredient}
+        remove={removeIngredient}
+        count={ingredients[ingredient]}
+        type={ingredient} />)
   }
 
   return (
     <div className={classes.BellControls}>
       <strong>Ingredients</strong>
       {results}
-      <Button disabled={!canBuy}>Order</Button>
+      <Button disabled={!total} onClick={startOrdering}>Order</Button>
     </div>
   );
 }
