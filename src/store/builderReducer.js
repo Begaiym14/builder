2af760ffead0d444f970a7fb1334335b603b2838
sovvetbekovna.02@gmall.com <kaparbekovaariyana@gmail.com>
2021-05-04 14:52:59 +0600

    add and Remove delete

--- src/store/builderReducer.js
+++ src/store/builderReducer.js
@@ -7,17 +7,27 @@ const initialState = {
         white: 1,
         yellow: 1,
     },
-    price:200,
-};
 
+    price: 200,
+};
+const prices = {
+    blue: 1,
+    pink: 1,
+    purple: 1,
+    red: 1,
+    white: 1,
+    yellow: 1,
+};
 const builderReducer = (state = initialState, action) => {
     const newState = { ...state }
     switch (action.type) {
         case "ADD_INGREDIENT":
             newState.ingredients[action.ingredient]++;
+            newState.price += prices[action.ingredient];
             break;
         case "REMOVE_INGREDIENT":
             newState.ingredients[action.ingredient]--;
+            newState.price -= prices[action.ingredient];
             break;
         default:
             break;
