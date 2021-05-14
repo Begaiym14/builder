ccf8875a4fe0d0c16ac092ce2a390a3e133a6f5d
sovvetbekovna.02@gmall.com <kaparbekovaariyana@gmail.com>
2021-05-14 13:58:31 +0600

    added images Bellingredient.js

--- src/components/BellBuilder/BellIngredient/BellIngredient.js
+++ src/components/BellBuilder/BellIngredient/BellIngredient.js
@@ -1,12 +1,21 @@
 import classes from "./BellIngredient.module.css";
-import dumaiBackground from "../../../images/dumai.png";
-import kayBackground from "../../../images/kay.png";
+
+import cola from "../../../images/cola.png";
+import fanta from "../../../images/fanta.png";
+import flesh from "../../../images/flesh.png";
+import nitro from "../../../images/nitro.png";
+import pepsi from "../../../images/pepsi.png";
+import sprite from "../../../images/sprite.png";
 
 
-const BellIngredient = ({ type }) => {
+const BellIngredient = ({ type,fixed }) => {
   const types = {
-    dumai: { backgroundImage: `url(${dumaiBackground})`, width: "70px", height: "70px" },
-    kay: { backgroundImage: `url(${kayBackground})`, width: "70px", height: "70px" },
+    cola: { backgroundImage: `url(${cola})`, width: "180px", height: "180px" },
+    fanta: { backgroundImage: `url(${fanta})`, width: "70px", height: "70px" },
+    flesh: { backgroundImage: `url(${flesh})`, width: "70px", height: "70px" },
+    nitro: { backgroundImage: `url(${nitro})`, width: "70px", height: "70px" },
+    pepsi: { backgroundImage: `url(${pepsi})`, width: "70px", height: "70px" },
+    sprite: { backgroundImage: `url(${sprite})`, width: "70px", height: "70px" },
   };
   return (
     <div className={classes.BellIngredient} style={types[type]}></div>
