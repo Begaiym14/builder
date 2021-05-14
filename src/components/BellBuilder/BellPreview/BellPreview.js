7fcbc53c9a604a7692f6e964bafaab34060d514f
sovvetbekovna.02@gmall.com <kaparbekovaariyana@gmail.com>
2021-05-14 14:25:07 +0600

    changed BellPreview.js

--- src/components/BellBuilder/BellPreview/BellPreview.js
+++ src/components/BellBuilder/BellPreview/BellPreview.js
@@ -1,6 +1,6 @@
 import BellIngredient from "../BellIngredient/BellIngredient";
-
 import classes from "./BellPreview.module.css";
+
 
 const BellPreview = ({ drinks, price }) => {
   const result = [];
@@ -12,12 +12,7 @@ const BellPreview = ({ drinks, price }) => {
 
   return (
     <div className={classes.BellPreview}>
-      <div
-        className={classes.bell}>
-        <div className={classes.drink}>
-          {/* <img src={lenta} alt="#" /> */}
-          {result}
-        </div>
+      <div className={classes.bell}>
       </div>
       <div className={classes.price}>{price.toFixed(1)} som</div>
     </div>
@@ -25,3 +20,41 @@ const BellPreview = ({ drinks, price }) => {
 }
 
 export default BellPreview;
+
+
+
+
+
+
+
+
+
+
+
+// import BellIngredient from "../BellIngredient/BellIngredient";
+
+// import classes from "./BellPreview.module.css";
+
+// const BellPreview = ({ drinks, price }) => {
+//   const result = [];
+//   for (const drink in drinks) {
+//     for (let i = 0; i < drinks[drink]; i++) {
+//       result.push(<BellIngredient key={drink + i} type={drink} />)
+//     }
+//   }
+
+//   return (
+//     <div className={classes.BellPreview}>
+//       <div
+//         className={classes.bell}>
+//         <div className={classes.drink}>
+//           {/* <img src={lenta} alt="#" /> */}
+//           {result}
+//         </div>
+//       </div>
+//       <div className={classes.price}>{price.toFixed(1)} som</div>
+//     </div>
+//   );
+// }
+
+// export default BellPreview;
