feb3dc08141242f8096b17783a486e261d91b244
sovvetbekovna.02@gmall.com <kaparbekovaariyana@gmail.com>
2021-05-20 15:52:21 +0600

    delete 2 and 4 storngs

--- src/components/BellBuilder/BellControls/BellControl/BellControl.js
+++ src/components/BellBuilder/BellControls/BellControl/BellControl.js
@@ -1,7 +1,5 @@
 import { useDispatch } from "react-redux";
-// import { add, remove } from "../../../../store/actions/biulder";
 import Button from "../../../UI/Button/Button";
-// import BellIngredient from "../../BellIngredient/BellIngredient";
 import classes from "./BellControl.module.css";
 
 const BellControl = ({ type, count }) => {
@@ -28,15 +26,6 @@ const BellControl = ({ type, count }) => {
 
     </div>
   );
-  // return (
-  //   <div className={classes.BellControl}>
-  //     <Button onClick={() => dispatch(add(type))}>+</Button>
-  //     <div className={classes.drink}>
-  //       <BellIngredient type={type} fixed />
-  //     </div>
-  //     <Button onClick={() => dispatch(remove(type))} disabled={!count}>-</Button>
-  //   </div>
-  // );
 }
 
 export default BellControl;
