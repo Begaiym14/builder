00775440dd9d4d3c0099134efb5f3a6132d1b9ec
sovvetbekovna.02@gmall.com <kaparbekovaariyana@gmail.com>
2021-05-20 15:52:54 +0600

    changed picture in BellIngredient.js

--- src/components/BellBuilder/BellIngredient/BellIngredient.js
+++ src/components/BellBuilder/BellIngredient/BellIngredient.js
@@ -1,24 +1,26 @@
+import React from 'react';
 import classes from "./BellIngredient.module.css";
 
-import cola from "../../../images/cola.png";
-import fanta from "../../../images/fanta.png";
-import flesh from "../../../images/flesh.png";
-import nitro from "../../../images/nitro.png";
-import pepsi from "../../../images/pepsi.png";
-import sprite from "../../../images/sprite.png";
+import { ReactComponent as ReactColaSvg } from '../../../images/cola.svg';
+import { ReactComponent as ReactFantaSvg } from "../../../images/fanta.svg";
+import { ReactComponent as ReactFleshSvg } from "../../../images/flesh.svg";
+import { ReactComponent as ReactNitroSvg } from "../../../images/nitro.svg";
+import { ReactComponent as ReactPepsiSvg } from "../../../images/pepsi.svg";
+import { ReactComponent as ReactSpriteSvg } from "../../../images/sprite.svg";
 
 
-const BellIngredient = ({ type,fixed }) => {
+const BellIngredient = ({ type,fixed  }) => {
   const types = {
-    cola: { backgroundImage: `url(${cola})`, width: "70px", height: "70px" },
-    fanta: { backgroundImage: `url(${fanta})`, width: "70px", height: "70px" },
-    flesh: { backgroundImage: `url(${flesh})`, width: "70px", height: "70px" },
-    nitro: { backgroundImage: `url(${nitro})`, width: "70px", height: "70px" },
-    pepsi: { backgroundImage: `url(${pepsi})`, width: "70px", height: "70px" },
-    sprite: { backgroundImage: `url(${sprite})`, width: "70px", height: "70px" },
+    ReactColaSvg: { backgroundImage: `url(${ReactColaSvg})`, width: "70px", height: "70px" },
+    ReactFantaSvg: { backgroundImage: `url(${ReactFantaSvg})`, width: "70px", height: "70px" },
+    ReactFleshSvg: { backgroundImage: `url(${ReactFleshSvg})`, width: "70px", height: "70px" },
+    ReactNitroSvg: { backgroundImage: `url(${ReactNitroSvg})`, width: "70px", height: "70px" },
+    ReactPepsiSvg: { backgroundImage: `url(${ReactPepsiSvg})`, width: "70px", height: "70px" },
+    ReactSpriteSvg: { backgroundImage: `url(${ReactSpriteSvg})`, width: "70px", height: "70px" },
   };
+
   return (
-    <div className={classes.BellIngredient} style={types[type]}></div>
+      <div className={classes.BellIngredient} style={types[type]}></div>
   );
 }
 export default BellIngredient;
