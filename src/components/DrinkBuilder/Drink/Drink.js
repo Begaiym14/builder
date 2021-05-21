e4dca9dd965bf506aa08ebe894ef7d1cfe813366
sovvetbekovna.02@gmall.com <kaparbekovaariyana@gmail.com>
2021-05-21 16:02:25 +0600

    chnaged files

--- src/components/DrinkBuilder/Drink/Drink.js
+++ src/components/DrinkBuilder/Drink/Drink.js
@@ -2,21 +2,21 @@
 import React from "react";
 
 import classes from "./Drink.module.css";
-import colaBackground from "../../../images/cola.svg";
-import fantaBackground from "../../../images/fanta.svg";
-import fleshBackground from "../../../images/flesh.svg";
-import nitroBackground from "../../../images/nitro.svg";
-import pepsiBackground from "../../../images/pepsi.svg";
-import spriteBackground from "../../../images/sprite.svg";
+import colaBackground from "../../../images/cola1.png";
+import fantaBackground from "../../../images/fanta1.png";
+import fleshBackground from "../../../images/flash1.png";
+import nitroBackground from "../../../images/nitro1.png";
+import pepsiBackground from "../../../images/pepsi.png";
+import spriteBackground from "../../../images/sprite1.png";
 
 const Drink = ({ type }) => {
   const types = {
-    cola: { backgroundImage: `url(${colaBackground})`, width: "50px", height: "50px" },
-    fanta: { backgroundImage: `url(${fantaBackground})`, width: "50px", height: "50px" },
-    flesh: { backgroundImage: `url(${fleshBackground})`, width: "45px", height: "45px" },
-    nitro: { backgroundImage: `url(${nitroBackground})`, width: "45px", height: "45px" },
-    pepsi: { backgroundImage: `url(${pepsiBackground})`, width: "45px", height: "45px" },
-    sprite: { backgroundImage: `url(${spriteBackground})`, width: "45px", height: "45px" },
+    cola: { backgroundImage: `url(${colaBackground})`, width: "55px", height: "58px" },
+    fanta: { backgroundImage: `url(${fantaBackground})`, width: "55px", height: "55px" },
+    flesh: { backgroundImage: `url(${fleshBackground})`, width: "55px", height: "60px" },
+    nitro: { backgroundImage: `url(${nitroBackground})`, width: "38px", height: "59px" },
+    pepsi: { backgroundImage: `url(${pepsiBackground})`, width: "50px", height: "69px" },
+    sprite: { backgroundImage: `url(${spriteBackground})`, width: "45px", height: "58px" },
   };
  
   return ( 
